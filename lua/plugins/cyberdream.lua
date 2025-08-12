9c86817f7d26eb1f2d9efdab423e20ef768b9bb3
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-08-12 12:48:13 -0600

    Customize theme

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -1,11 +1,11 @@
 -- plugins/cyberdream.lua or inside your Lazy spec
 return {
-	"scottmckendry/cyberdream.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		theme = "auto",
-		transparent = true,
-		disable_autocmd = true, -- 👈 disables auto-colorscheme
-	},
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    theme = "auto",
+    -- transparent = true,
+    disable_autocmd = true, -- 👈 disables auto-colorscheme
+  },
 }
