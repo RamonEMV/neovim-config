c91f4a824c4e65ad1f19e57132a635a369993eae
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-09-26 13:21:14 -0600

    Add current location to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,13 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
-    theme = 'ayu_light',
-  }
+    options = {
+      theme = 'material',
+    },
+    sections = {
+      lualine_c = {
+        { 'filename', path = 1 }, -- 0 = filename, 1 = relative path, 2 = absolute path
+      },
+    },
+  },
 }
