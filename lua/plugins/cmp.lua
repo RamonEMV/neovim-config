d12266b61b4de6543dd9f24b02e71c0af97736f6
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-09-26 13:22:14 -0600

    Improve autocomplete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,71 +1,75 @@
 return {
-	-- Main completion engine
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			-- Completion sources
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
+  -- Main completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      -- Completion sources
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
 
-			-- Snippet engine & sources
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
+      -- Snippet engine & sources
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
 
-			-- Autocomplete icons
-			"onsails/lspkind.nvim",
-		},
-		config = function()
-			local lspkind = require("lspkind")
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
+      -- Autocomplete icons
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local lspkind = require("lspkind")
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol_text", -- show symbol + text (e.g.,  function)
-						maxwidth = 50, -- prevent long entries
-						ellipsis_char = "...", -- if maxwidth is exceeded
-					}),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = cmp.mapping.select_next_item(),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",  -- show symbol + text (e.g.,  function)
+            maxwidth = 50,         -- prevent long entries
+            ellipsis_char = "...", -- if maxwidth is exceeded
+          }),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-c>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          -- { name = "buffer" },
+          -- { name = "path" },
+        }),
+        completion = {
+          autocomplete = { require("cmp.types").cmp.TriggerEvent.TextChanged },
+        },
+      })
 
-			-- Enable completion in command line
-			cmp.setup.cmdline("/", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" }
-				}
-			})
+      -- Enable completion in command line
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
 
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" }
-				}, {
-					{ name = "cmdline" }
-				})
-			})
-		end,
-	},
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          { name = "cmdline" }
+        })
+      })
+    end,
+  },
 }
