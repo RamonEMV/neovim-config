10035cccd70aaa419b2f166d632b22f427c6d58a
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-09-26 13:21:47 -0600

    Improve autocomplete

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,12 +1,12 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "vue_ls",
-	  "vtsls"
-        }
-      })
-    end
-  }
+  "williamboman/mason-lspconfig.nvim",
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        -- "vue_ls",
+        -- "vtsls"
+      }
+    })
+  end
+}
