3a91b318f894761100c81d36cb01e4e7eb3426f4
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-10-01 15:50:31 -0600

    Update icons

--- lua/plugins/lsp-kind.lua
+++ lua/plugins/lsp-kind.lua
@@ -1,37 +1,37 @@
 return {
-	{
-		"onsails/lspkind.nvim",
-		lazy = true, -- Optional: load lazily
-		event = "VeryLazy", -- or remove to load immediately
-		config = function()
-			require("lspkind").init({
-				-- optional configuration
-				mode = "symbol_text", -- show symbol + text
-				preset = "codicons",
-				symbol_map = {
-					Text = "",
-					Method = "ƒ",
-					Function = "",
-					Constructor = "",
-					Variable = "",
-					Class = "",
-					Interface = "ﰮ",
-					Module = "",
-					Property = "",
-					Unit = "",
-					Value = "",
-					Enum = "了",
-					Keyword = "",
-					Snippet = "﬌",
-					Color = "",
-					File = "",
-					Reference = "",
-					Folder = "",
-					EnumMember = "",
-					Constant = "",
-					Struct = "",
-				},
-			})
-		end,
-	},
+  {
+    "onsails/lspkind.nvim",
+    lazy = true,        -- Optional: load lazily
+    event = "VeryLazy", -- or remove to load immediately
+    config = function()
+      require("lspkind").init({
+        -- optional configuration
+        mode = "symbol_text", -- show symbol + text
+        preset = "codicons",
+        symbol_map = {
+          Text = "",
+          Method = "ƒ",
+          Function = "",
+          Constructor = "",
+          Variable = "",
+          Class = "",
+          Interface = "󱘖",
+          Module = "󰕳",
+          Property = "",
+          Unit = "",
+          Value = "",
+          Enum = "了",
+          Keyword = "",
+          Snippet = "",
+          Color = "",
+          File = "",
+          Reference = "",
+          Folder = "",
+          EnumMember = "",
+          Constant = "",
+          Struct = "",
+        },
+      })
+    end,
+  },
 }
