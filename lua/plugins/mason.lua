d1e7a6d21335cced37eb0bbb5d3b0ae06869a6dd
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-10-07 13:05:48 -0600

    Add new plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,10 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+  "mason-org/mason.nvim",
+  opts = {
+    registries = {
+      "github:mason-org/mason-registry",   -- default registry
+      "github:Crashdummyy/mason-registry", -- your custom registry
+      -- "github:seblyng/roslyn.nvim",        -- your custom registry
+    },
+  }
 }
