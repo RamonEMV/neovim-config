42347382c038d12c26a07326999bb1065a46ee5d
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-10-10 17:32:17 -0600

    Customize bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,11 +8,35 @@ return {
       options = {
         mode = "buffers", -- or "tabs"
         diagnostics = "nvim_lsp",
-        separator_style = "slant",
+        separator_style = "padded_sloped",
         show_buffer_close_icons = false,
         show_close_icon = false,
         view = "tabs",
+        enforce_regular_tabs = true,
+        -- tab_size = 20,
+        color_icons = true,
       },
+      highlights = {
+        -- Active tab
+        buffer_selected = {
+          bold = true,
+          italic = false,
+          underline = true,
+        },
+        -- Inactive tabs
+        background = {
+          italic = true,
+          fg = { attribute = "fg", highlight = "Comment" },
+          bg = { attribute = "bg", highlight = "NormalNC" },
+        },
+        -- Active tab background (stronger)
+        fill = {
+          bg = { attribute = "bg", highlight = "StatusLine" },
+        },
+        separator_selected = {
+          fg = { attribute = "fg", highlight = "Directory" },
+        },
+      }
     })
 
     -- optional keymaps
