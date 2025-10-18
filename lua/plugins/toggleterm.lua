05b673af9d72ab6936da7c06848f21eae7d3ae53
Ramón Melchor <ramon_melchorzx@hotmail.com>
2025-10-17 18:48:51 -0600

    Add delete terminal command

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -74,11 +74,37 @@ return {
         end
       end
 
+      -- Delete current terminal
+      function _DELETE_TERM()
+        if #terminals == 0 then return end
+
+        -- Close and remove current terminal
+        local term = terminals[current]
+        if term:is_open() then
+          term:toggle()
+        end
+
+        table.remove(terminals, current)
+
+        -- Adjust current index
+        if current > #terminals then
+          current = #terminals
+        end
+
+        -- If there are still terminals, show the next one
+        if #terminals > 0 then
+          _SHOW_TERM(current)
+        else
+          current = 0
+        end
+      end
+
       -- Keybindings
       vim.keymap.set("n", "<leader>tc", _CREATE_TERM, { silent = true, desc = "Create terminal" })
       vim.keymap.set("n", "<leader>tn", _NEXT_TERM, { silent = true, desc = "Next terminal" })
       vim.keymap.set("n", "<leader>tp", _PREV_TERM, { silent = true, desc = "Previous terminal" })
       vim.keymap.set("n", "<leader>tt", _TOGGLE_TERM, { silent = true, desc = "Toggle terminal" })
+      vim.keymap.set("n", "<leader>td", _DELETE_TERM, { silent = true, desc = "Delete terminal" })
     end
   }
 }
